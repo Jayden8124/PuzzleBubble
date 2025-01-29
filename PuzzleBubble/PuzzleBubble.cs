ed63676f5dd61d850e8e6cdcf072ab6b519db947
Petchz87 <zpetza1547@gmail.com>
2025-01-29 12:09:15 +0700

    spritesheet

--- PuzzleBubble/PuzzleBubble.cs
+++ PuzzleBubble/PuzzleBubble.cs
@@ -24,7 +24,7 @@ public class PuzzleBubble : Game
     Texture2D _rect; // Play Zone
     SpriteFont _font; // Font
     // Size Play Zone
-    public const int _PlayWidth = 780;  
+    public const int _PlayWidth = 780;
     public const int _PlayHeight = 915;
     public const int _scoreboardWidth = 500;
     public const int _scoreboardHeight = 100;
@@ -32,6 +32,9 @@ public class PuzzleBubble : Game
     public const int _barNameHeight = 338;
     public const int _pictureBossWidth = 279;
     public const int _pictureBossHeight = 367;
+
+    List<GameObject> _gameObjects;
+    int _numObjects;
 
     // test
     int _score;
@@ -63,9 +66,10 @@ public class PuzzleBubble : Game
         _graphics.PreferredBackBufferHeight = 1080;
 
         _graphics.ApplyChanges();
+        _gameObjects = new List<GameObject>();
 
         // TODO: Add your initialization logic here
-        
+
         base.Initialize();
     }
 
@@ -79,58 +83,59 @@ public class PuzzleBubble : Game
         // Create a 1x1 pixel texture for drawing lines
         _lineTexture = new Texture2D(GraphicsDevice, 1, 1);
         _lineTexture.SetData(new[] { Color.White });
+        Reset();
 
 
-        // Bubble Texture
-        {
-            _b1 = Content.Load<Texture2D>("b1");
-            _b2 = Content.Load<Texture2D>("b2");
-            _b3 = Content.Load<Texture2D>("b3");
-            _br1 = Content.Load<Texture2D>("br1");
-            _br2 = Content.Load<Texture2D>("br2");
-            _br3 = Content.Load<Texture2D>("br3");
-            _r1 = Content.Load<Texture2D>("r1");
-            _r2 = Content.Load<Texture2D>("r2");
-            _r3 = Content.Load<Texture2D>("r3");
-            _y1 = Content.Load<Texture2D>("y1");
-            _y2 = Content.Load<Texture2D>("y2");
-            _y3 = Content.Load<Texture2D>("y3");
-        }
+        // // Bubble Texture
+        // {
+        //     _b1 = Content.Load<Texture2D>("b1");
+        //     _b2 = Content.Load<Texture2D>("b2");
+        //     _b3 = Content.Load<Texture2D>("b3");
+        //     _br1 = Content.Load<Texture2D>("br1");
+        //     _br2 = Content.Load<Texture2D>("br2");
+        //     _br3 = Content.Load<Texture2D>("br3");
+        //     _r1 = Content.Load<Texture2D>("r1");
+        //     _r2 = Content.Load<Texture2D>("r2");
+        //     _r3 = Content.Load<Texture2D>("r3");
+        //     _y1 = Content.Load<Texture2D>("y1");
+        //     _y2 = Content.Load<Texture2D>("y2");
+        //     _y3 = Content.Load<Texture2D>("y3");
+        // }
 
-        // Player Texture
-        {
-            _h1 = Content.Load<Texture2D>("h1");
-            _h2 = Content.Load<Texture2D>("h2");
-        }
+        // // Player Texture
+        // {
+        //     _h1 = Content.Load<Texture2D>("h1");
+        //     _h2 = Content.Load<Texture2D>("h2");
+        // }
 
-        // Rectangle Texture
-        {
-            _rect = new Texture2D(_graphics.GraphicsDevice, 1, 1);
-            Color[] data = new Color[1];
-            data[0] = Color.Black;
-            _rect.SetData(data);
-        }
+        // // Rectangle Texture
+        // {
+        //     _rect = new Texture2D(_graphics.GraphicsDevice, 1, 1);
+        //     Color[] data = new Color[1];
+        //     data[0] = Color.Black;
+        //     _rect.SetData(data);
+        // }
 
-        // โหลดไฟล์ภาพจาก Content Pipeline
-        _bubbleTextures = new List<Texture2D>
-        {
-            Content.Load<Texture2D>("b1"),
-            Content.Load<Texture2D>("b2"),
-            Content.Load<Texture2D>("b3"),
-            Content.Load<Texture2D>("br1"),
-            Content.Load<Texture2D>("br2"),
-            Content.Load<Texture2D>("br3"),
-            Content.Load<Texture2D>("r1"),
-            Content.Load<Texture2D>("r2"),
-            Content.Load<Texture2D>("r3"),
-            Content.Load<Texture2D>("y1"),
-            Content.Load<Texture2D>("y2"),
-            Content.Load<Texture2D>("y3"),
-        };
+        // // โหลดไฟล์ภาพจาก Content Pipeline
+        // _bubbleTextures = new List<Texture2D>
+        // {
+        //     Content.Load<Texture2D>("b1"),
+        //     Content.Load<Texture2D>("b2"),
+        //     Content.Load<Texture2D>("b3"),
+        //     Content.Load<Texture2D>("br1"),
+        //     Content.Load<Texture2D>("br2"),
+        //     Content.Load<Texture2D>("br3"),
+        //     Content.Load<Texture2D>("r1"),
+        //     Content.Load<Texture2D>("r2"),
+        //     Content.Load<Texture2D>("r3"),
+        //     Content.Load<Texture2D>("y1"),
+        //     Content.Load<Texture2D>("y2"),
+        //     Content.Load<Texture2D>("y3"),
+        // };
 
-        // สร้างฟองอากาศ
-        _bubbles = new List<Bubble>();
-        InitializeBubble();
+        // // สร้างฟองอากาศ
+        // _bubbles = new List<Bubble>();
+        // InitializeBubble();
     }
 
     protected override void Update(GameTime gameTime)
@@ -139,7 +144,22 @@ public class PuzzleBubble : Game
         //     Exit();
 
         MouseState state = Mouse.GetState();
-
+        //update
+        _numObjects = _gameObjects.Count;
+        for (int i = 0; i < _numObjects; i++)
+        {
+            if (_gameObjects[i].IsActive)
+                _gameObjects[i].Update(gameTime, _gameObjects);
+        }
+        for (int i = 0; i < _numObjects; i++)
+        {
+            if (!_gameObjects[i].IsActive)
+            {
+                _gameObjects.RemoveAt(i);
+                i--;
+                _numObjects--;
+            }
+        }
 
         //     switch (_currentGameState)
         // {
@@ -175,14 +195,20 @@ public class PuzzleBubble : Game
         GraphicsDevice.Clear(Color.White);
 
         _spriteBatch.Begin();
+        _numObjects = _gameObjects.Count;
 
-        // Base Background
-        _spriteBatch.Draw(_rect, new Vector2(515, 65), null, Color.Black, 0f, Vector2.Zero, new Vector2(_PlayWidth, _PlayHeight), SpriteEffects.None, 0f);
-        _spriteBatch.Draw(_rect, new Vector2(1360, 100), null, Color.Black, 0f, Vector2.Zero, new Vector2(_scoreboardWidth, _scoreboardHeight), SpriteEffects.None, 0f);
-        _spriteBatch.Draw(_rect, new Vector2(1360, 220), null, Color.Black, 0f, Vector2.Zero, new Vector2(_scoreboardWidth, _scoreboardHeight), SpriteEffects.None, 0f);
-        _spriteBatch.Draw(_rect, new Vector2(80, 90), null, Color.Black, 0f, Vector2.Zero, new Vector2(_barNameHeight, _barNameWidth), SpriteEffects.None, 0f);
-        _spriteBatch.Draw(_rect, new Vector2(70, 165), null, Color.Black, 0f, Vector2.Zero, new Vector2(_pictureBossHeight, _pictureBossWidth), SpriteEffects.None, 0f);
-        _spriteBatch.DrawCircle(new Vector2(1449, 417), 50, 100, Color.Red, 50);
+        for (int i = 0; i < _numObjects; i++)
+        {
+            _gameObjects[i].Draw(_spriteBatch);
+        }
+
+        // // Base Background
+        // _spriteBatch.Draw(_rect, new Vector2(515, 65), null, Color.Black, 0f, Vector2.Zero, new Vector2(_PlayWidth, _PlayHeight), SpriteEffects.None, 0f);
+        // _spriteBatch.Draw(_rect, new Vector2(1360, 100), null, Color.Black, 0f, Vector2.Zero, new Vector2(_scoreboardWidth, _scoreboardHeight), SpriteEffects.None, 0f);
+        // _spriteBatch.Draw(_rect, new Vector2(1360, 220), null, Color.Black, 0f, Vector2.Zero, new Vector2(_scoreboardWidth, _scoreboardHeight), SpriteEffects.None, 0f);
+        // _spriteBatch.Draw(_rect, new Vector2(80, 90), null, Color.Black, 0f, Vector2.Zero, new Vector2(_barNameHeight, _barNameWidth), SpriteEffects.None, 0f);
+        // _spriteBatch.Draw(_rect, new Vector2(70, 165), null, Color.Black, 0f, Vector2.Zero, new Vector2(_pictureBossHeight, _pictureBossWidth), SpriteEffects.None, 0f);
+        // _spriteBatch.DrawCircle(new Vector2(1449, 417), 50, 100, Color.Red, 50);
 
         // SCORE Drawing
         _spriteBatch.DrawString(_font, "SCORE: " + _score, new Vector2(1449, 266), Color.White);
@@ -196,7 +222,7 @@ public class PuzzleBubble : Game
         MouseState mouseState = Mouse.GetState();
         int mouseX = mouseState.X;
         int mouseY = mouseState.Y;
-        
+
         // Draw horizontal and vertical lines
         _spriteBatch.Draw(_lineTexture, new Rectangle(0, mouseY, _graphics.PreferredBackBufferWidth, 1), Color.Red); // Horizontal line
         _spriteBatch.Draw(_lineTexture, new Rectangle(mouseX, 0, 1, _graphics.PreferredBackBufferHeight), Color.Red); // Vertical line
@@ -257,6 +283,145 @@ public class PuzzleBubble : Game
 
                 _bubbles.Add(newBubble); // add Bubble To List
             }
+        }
+    }
+
+    protected void Reset()
+    {
+        // Singleton.Instance.Score = 0;
+        // Singleton.Instance.Life = 3;
+
+        // Singleton.Instance.CurrentGameState = Singleton.GameState.StartNewLife;
+
+        // Singleton.Instance.Random = new System.Random();
+
+        Texture2D spaceInvaderTexture = Content.Load<Texture2D>("Sprite");
+
+        _gameObjects.Clear();
+        _gameObjects.AddRange(new List<GameObject>
+        {
+            new bb(spaceInvaderTexture)
+            {
+                Name = "Player",
+                Viewport = new Rectangle(22, 132, 70, 70),
+                Position = new Vector2(50, 50),
+                Left = Keys.Left,
+                Right = Keys.Right,
+                Fire = Keys.Space,
+            },
+            new bb(spaceInvaderTexture)
+            {
+                Name = "Player2",
+                Viewport = new Rectangle(112, 136, 70, 70),
+                Position = new Vector2(150, 50),
+                Left = Keys.Left,
+                Right = Keys.Right,
+                Fire = Keys.Space,
+            },
+            new bb(spaceInvaderTexture)
+            {
+                Name = "Player3",
+                Viewport = new Rectangle(204, 142, 70, 70),
+                Position = new Vector2(250, 50),
+                Left = Keys.Left,
+                Right = Keys.Right,
+                Fire = Keys.Space,
+            },  
+            new bb(spaceInvaderTexture)
+            {
+                Name = "Player",
+                Viewport = new Rectangle(18, 350, 70, 70),
+                Position = new Vector2(50, 150),
+                Left = Keys.Left,
+                Right = Keys.Right,
+                Fire = Keys.Space,
+            },
+            new bb(spaceInvaderTexture)
+            {
+                Name = "Player2",
+                Viewport = new Rectangle(110, 355, 70, 70),
+                Position = new Vector2(150, 150),
+                Left = Keys.Left,
+                Right = Keys.Right,
+                Fire = Keys.Space,
+            },
+            new bb(spaceInvaderTexture)
+            {
+                Name = "Player3",
+                Viewport = new Rectangle(204, 359, 70, 70),
+                Position = new Vector2(250, 150),
+                Left = Keys.Left,
+                Right = Keys.Right,
+                Fire = Keys.Space,
+            }, 
+            new bb(spaceInvaderTexture)
+            {
+                Name = "Player",
+                Viewport = new Rectangle(18, 241, 70, 70),
+                Position = new Vector2(50, 350),
+                Left = Keys.Left,
+                Right = Keys.Right,
+                Fire = Keys.Space,
+            },
+            new bb(spaceInvaderTexture)
+            {
+                Name = "Player2",
+                Viewport = new Rectangle(110, 248, 70, 70),
+                Position = new Vector2(450, 250),
+                Left = Keys.Left,
+                Right = Keys.Right,
+                Fire = Keys.Space,
+            },
+            new bb(spaceInvaderTexture)
+            {
+                Name = "Player3",
+                Viewport = new Rectangle(204, 248, 70, 70),
+                Position = new Vector2(550, 250),
+                Left = Keys.Left,
+                Right = Keys.Right,
+                Fire = Keys.Space,
+            },
+            new bb(spaceInvaderTexture)
+            {
+                Name = "gun",
+                Viewport = new Rectangle(20, 798, 300, 150),
+                Position = new Vector2(550, 550),
+                Left = Keys.Left,
+                Right = Keys.Right,
+                Fire = Keys.Space,
+            },
+            new bb(spaceInvaderTexture)
+            {
+                Name = "gun2",
+                Viewport = new Rectangle(20, 1478, 300, 150),
+                Position = new Vector2(980, 750),
+                Left = Keys.Left,
+                Right = Keys.Right,
+                Fire = Keys.Space,
+            },
+            new bb(spaceInvaderTexture)
+            {
+                Name = "gun3",
+                Viewport = new Rectangle(20, 1140, 300, 150),
+                Position = new Vector2(250, 800),
+                Left = Keys.Left,
+                Right = Keys.Right,
+                Fire = Keys.Space,
+            },       
+            new bb(spaceInvaderTexture)
+            {
+                Name = "player",
+                Viewport = new Rectangle(26, 573, 190, 180),
+                Position = new Vector2(650, 850),
+                Left = Keys.Left,
+                Right = Keys.Right,
+                Fire = Keys.Space,
+            },       
+        });
+
+        foreach (GameObject s in _gameObjects)
+        {
+            s.Reset();
         }
     }
 
